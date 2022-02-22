22d5f42965b8718aa960067aef3ae715dcaab442
EslamBanna <solombana2000@gmail.com>
2022-02-23 01:13:43 +0200

    start in pdf

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -9,11 +9,13 @@ use App\Models\Answer;
 use App\Models\Form;
 use App\Models\Question;
 use Excel;
+use PDF;
 
 class ExportController extends Controller
 {
     use GeneralTrait;
-    public function exportExcel($formId)
+
+    private function prepareData($formId)
     {
         try {
             $form_all = Form::with('submits.answers')->find($formId);
@@ -48,7 +50,21 @@ class ExportController extends Controller
                 }
             }
             $form_questions = Question::select('id', 'question')->where('form_id', $formId)->get();
-            return Excel::download(new ResponsesExport($form_questions, collect($response)), 'test.xlsx');
+            $data = [
+                'responses' => $response,
+                'form_questions' => $form_questions
+            ];
+            return $data;
+        } catch (\Exception $e) {
+            return $this->returnError(201, $e->getMessage());
+        }
+    }
+
+    public function exportExcel($formId)
+    {
+        try {
+            $data = $this->prepareData($formId);
+            return Excel::download(new ResponsesExport($data['form_questions'], collect($data['responses'])), 'test.xlsx');
         } catch (\Exception $e) {
             return $this->returnError(201, $e->getMessage());
         }
@@ -58,42 +74,25 @@ class ExportController extends Controller
     public function exportPdf($formId)
     {
         try {
-            $form_all = Form::with('submits.answers')->find($formId);
-            $form_counted = Form::with(['submits.answers' => function ($q) {
-                $q->select('*')->selectRaw('count(question_id) as answer_len')->groupBy(['question_id', 'submit_id']);
-            }])->find($formId);
-            if (!$form_all) {
-                return $this->returnError(202, 'this form does not exist');
-            }
-            $response = [];
-            $pointer = 0;
-            foreach ($form_counted->submits as $ind => $submit) {
-                $pointer = 0;
-                foreach ($submit->answers as $index => $answer) {
-                    $te = '';
-                    if ($answer['answer_len'] > 1) {
-                        for ($i = 0; $i < $answer['answer_len']; $i++) {
-                            if ($i == 0) {
-                                $te = $form_all['submits'][$ind]['answers'][$pointer]['answer'];
-                            } else {
-                                $te = $te . ',' . $form_all['submits'][$ind]['answers'][$pointer]['answer'];
-                            }
-                            $pointer++;
-                            if ($i == $answer['answer_len'] - 1) {
-                                $response[$ind]['test' . $index] = $te;
-                            }
-                        }
-                    } else {
-                        $pointer++;
-                        $response[$ind]['test' . $index] = $answer['answer'];
-                    }
-                }
-            }
-            $form_questions = Question::select('id', 'question')->where('form_id', $formId)->get();
-            return Excel::download(new ResponsesExport($form_questions, collect($response)), 'test.pdf');
+            $data = $this->prepareData($formId);
+            $response = $data['responses'];
+            $form_questions = $data['form_questions'];
+            return view('pdf.pdf', compact('response', 'form_questions','formId'));
         } catch (\Exception $e) {
             return $this->returnError(201, $e->getMessage());
         }
     }
-    
+
+    public function covertHTMlToPdf($formId)
+    {
+        try {
+            $data = $this->prepareData($formId);
+            view()->share('pdf.pdf', $data['responses']);
+            $pdf = PDF::loadView('pdf.pdf', $data['responses']);
+            // download PDF file with download method
+            return $pdf->download('pdf_file.pdf');
+        } catch (\Exception $e) {
+            return $this->returnError(201, $e->getMessage());
+        }
+    }
 }
